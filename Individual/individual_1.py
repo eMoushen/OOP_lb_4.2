172b260c5c17f6fedf9248994946ecb28538b10e
Dmitry <strigalov1991@mail.ru>
2024-01-27 19:37:29 +0300

    update

--- Individual/individual_1.py
+++ Individual/individual_1.py
@@ -15,25 +15,23 @@ class MyPair:
     def value1(self):
         return self.__value1
 
+    @value1.setter
+    def value1(self, value):
+        self.__value1 = float(value)
+
     @property
     def value2(self):
         return self.__value2
 
-    # Прочитать значение
-    def read(self):
-        self.__value1 = float(input("Введите значение для первого поля (дробное число): "))
-        self.__value2 = float(input("Введите значение для второго поля (положительное дробное число): "))
-        if self.__value2 <= 0:
-            raise ValueError("Значение второго поля должно быть положительным дробным числом")
+    @value2.setter
+    def value2(self, value):
+        self.__value2 = float(value)
 
-    # Вывести на экран
-    def display(self):
-        print(f"Value1: {self.__value1}, Value2: {self.__value2}")
+    def __str__(self):
+        return f"Value1: {self.__value1}, Value2: {self.__value2}"
 
-    # Умножение на произвольное дробное число
-    def multiply(self, factor):
-        self.__value1 *= factor
-        self.__value2 *= factor
+    def __mul__(self, factor):
+        return MyPair(self.__value1 * factor, self.__value2 * factor)
 
     def __eq__(self, other):
         return self.__value1 == other.__value1 and self.__value2 == other.__value2
@@ -41,38 +39,13 @@ class MyPair:
     def __ne__(self, other):
         return self.__value1 != other.__value1 or self.__value2 != other.__value2
 
-    def __mul__(self, factor):
-        return MyPair(self.__value1 * factor, self.__value2 * factor)
-
-
-def make_pair(first, second):
-    """
-    Функция создания экземпляра класса MyPair, принимая значения полей как аргументы
-    """
-    return MyPair(first, second)
-
 
 if __name__ == '__main__':
-    pair = MyPair()
-    pair.read()
-    pair.display()
-
+    pair = MyPair(3, 2)
     factor = float(input("Введите множитель для умножения: "))
-    pair.multiply(factor)
-    print("Результат умножения:")
-    pair.display()
+    result_pair = pair * factor
+    print(f"Результат умножения: {result_pair}")
 
-    other_pair = MyPair()
-    other_pair.read()
-
-    # Перегрузка оператора ==
-    print(pair == other_pair)
-
-    # Перегрузка оператора !=
-    print(pair != other_pair)
-
-    # Перегрузка оператора умножения *
-    factor = float(input("Введите множитель для умножения 2-ого экземпляра: "))
-    result_pair = other_pair * factor
-    print("Результат умножения 2-ого экземпляра:")
-    result_pair.display()
+    pair2 = MyPair(3, 2)
+    print(f"Сравнение пар на равенство: {pair == pair2}")
+    print(f"Сравнение пар на неравенство: {pair != pair2}")
